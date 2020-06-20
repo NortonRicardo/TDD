8bd2ecd29890800d5cc640384d5eeb0acbf8457c
NORTON RICARDO <norton@marbocontabil.com.br>
2020-06-20 19:44:45 -0300

    create tdd model presence

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :description, presence: { message: 'Descrição está em branco!' }
+  validates :category, presence: { message: 'Categoria está em branco!' }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with description, price and category' do
+    product = create(:product)
+    expect(product).to be_valid
+  end
+
+  it 'is valid with description blank' do
+    product = build(:product, description: nil)
+    product.valid?
+    expect(product.errors[:description]).to include('Descrição está em branco!')
+  end
+
+  it 'is valid with category blank' do
+    product = build(:product, category: nil)
+    product.valid?
+    puts product.errors[:category]
+    expect(product.errors[:category]).to include('Categoria está em branco!')
+  end
 end

f5d189a13345c8d0760ea79cfffd24656a350470
NORTON RICARDO <norton@marbocontabil.com.br>
2020-06-20 13:30:43 -0300

    new teste

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -2,9 +2,30 @@
 
 FactoryBot.define do
   factory :customer, aliases: [:user] do
+    transient do
+      upcased false
+      downcased true
+    end
+
     name Faker::Name.name
     email Faker::Internet.email
-    vip true
-    days_to_pay 30
+
+    factory :customer_vip do
+      vip true
+      days_to_pay 30
+    end
+
+    factory :customer_default do
+      vip false
+      days_to_pay 10
+    end
+
+    after(:create) do |customer, evaluator|
+      customer.name.upcase! if evaluator.upcased
+    end
+
+    after(:create) do |customer, evaluator|
+      customer.email.downcase! if evaluator.downcased
+    end
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Customer, type: :model do
   end
 
   it 'Customer Teste vip' do
-    customer = create(:user)
+    customer = create(:customer_vip)
     expect(customer.vip).not_to be_falsey
   end
 
@@ -19,4 +19,19 @@ RSpec.describe Customer, type: :model do
   end
 
   it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
+
+  it 'Usando attributes For' do
+    attrs = attributes_for(:customer)
+    puts attrs
+  end
+
+  it 'create customer email dowcase' do
+    customer = create(:customer_vip, email: 'NORTON@NORTON.COM')
+    expect(customer.email).to eq(customer.email.downcase)
+  end
+
+  it 'Atributo Transitorio' do
+    customer = create(:customer_vip, upcased: true)
+    expect(customer.name.upcase).to eq(customer.name)
+  end
 end
